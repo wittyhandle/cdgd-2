3f0f4a1416bcb4af0fe24f4b84025950c10693e0
Mike Ottinger <mikeottinger@gmail.com>
2019-09-02 11:48:59 -0700

    Added state to the login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = () => {
-    return (
-        <div className={'row align-self-center w-100'}>
-            <div className={'col'}>
-                <form className={'mx-auto w-50 p-3'}>
-                    <div className={'form-group'}>
 
-                        <div className={'cdgd-field'}>
-                            <label htmlFor='username'>Username</label>
-                            <input type='text' className={'form-control'} id='username'/>
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.id] : e.target.value
+        })
+    };
+
+    doLogin = (e) => {
+        e.preventDefault()
+        console.log('will do login with', this.state);
+    };
+
+    render() {
+        return (
+            <div className={'row align-self-center w-100'}>
+                <div className={'col'}>
+                    <form className={'mx-auto w-50 p-3'} onSubmit={this.doLogin}>
+                        <div className={'form-group'}>
+
+                            <div className={'cdgd-field'}>
+                                <label htmlFor='username'>Username</label>
+                                <input type='text' value={this.state.username} className={'form-control'} onChange={this.handleOnChange} id='username'/>
+                            </div>
+
+                            <div className={'cdgd-field'}>
+                                <label htmlFor='password'>Password</label>
+                                <input type='password' value={this.state.password} className={'form-control'} onChange={this.handleOnChange} id='password'/>
+                            </div>
+
+                            <div className={'cdgd-button'}>
+                                <button type='submit' className={'btn btn-primary'}>Submit</button>
+                            </div>
                         </div>
-
-                        <div className={'cdgd-field'}>
-                            <label htmlFor='password'>Password</label>
-                            <input type='password' className={'form-control'} id='password'/>
-                        </div>
-
-                        <div className={'cdgd-button'}>
-                            <button type='submit' className={'btn btn-primary'}>Submit</button>
-                        </div>
-                    </div>
-
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Login;
