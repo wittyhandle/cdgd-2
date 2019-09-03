93df51961b714c5c1c958e2e80bc2242fddc80ff
Mike Ottinger <mikeottinger@gmail.com>
2019-09-02 19:25:14 -0700

    Added a simple authenticate endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,18 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+router.post('/authenticate', function(req, res, next) {
+
+  const USERS = [
+    {username: 'Mike', password: 'secret'}
+  ];
+
+  let user = USERS.find(u => u.username === req.body.username && u.password === req.body.password);
+  if (!user) {
+    res.status('401').json({success: false, message: 'Invalid Credentials'});
+  } else {
+    res.json({success: true, message: 'Successful Login'});
+  }
 });
 
 module.exports = router;
