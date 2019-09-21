a0ae387b0b9da3352c47b7ee8de937071cd438c2
Mike Ottinger <mikeottinger@gmail.com>
2019-09-21 10:30:01 -0700

    First pass at nav bar

--- src/components/layouts/WithSidebar.js
+++ src/components/layouts/WithSidebar.js
@@ -12,6 +12,20 @@ export const WithSidebar = props => {
 
             <div className={'main-panel'}>
 
+                <nav className={'navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent'}>
+                    <div className={'container-fluid'}>
+                        <div className={'navbar-wrapper'}>
+                            <div className={'navbar-brand'}>CDGD Materials</div>
+                        </div>
+                        <div className={'collapse navbar-collapse justify-content-end'} id={'navigation'}>
+                            <ul className={'navbar-nav'}>
+                                <li className={'nav-item'}>
+                                    <a className={'nav-link'} href={'#'}>Mike - Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
 
                 <div className={'content'}>
                     Main
