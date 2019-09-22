c72702a3b0c67cc7913a439411f397d324e6cb1a
Mike Ottinger <mikeottinger@gmail.com>
2019-09-21 18:40:26 -0700

    Added drop-down html

--- src/components/layouts/WithSidebar.js
+++ src/components/layouts/WithSidebar.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Footer } from '../index';
 import { AuthenticationConsumer } from '../../context/authentication.context';
 
 export const WithSidebar = props => {
+
+    const [dropdownState, setDropdownState] = useState({
+        cssClass: '', isExpanded: false
+    });
+
+    const onClick = () => {
+        const klass = dropdownState.cssClass === '' ? 'show' : '';
+        setDropdownState({cssClass: klass, isExpanded: !dropdownState.isExpanded});
+    };
 
     return (
 
@@ -22,13 +31,21 @@ export const WithSidebar = props => {
                                 </div>
                                 <div className={'collapse navbar-collapse justify-content-end'} id={'navigation'}>
                                     <ul className={'navbar-nav'}>
-                                        <li className={'nav-item'}>
-                                            <a className={'nav-link'} href={'#'}>
-                                                <i className={'nc-icon nc-single-02'}/>
+                                        <li className={'nav-item btn-rotate dropdown ' + dropdownState.cssClass }>
+                                            <a className={'nav-link dropdown-toggle'} href={'#'} onClick={onClick} data-toggle='dropdown' aria-expanded={dropdownState.isExpanded}>
+                                                <i className={'nc-icon'}/>
                                                 <p>
                                                     <span className={'d-md-block'}>{currentUser}</span>
                                                 </p>
                                             </a>
+                                            <div className={'dropdown-menu dropdown-menu-right ' + dropdownState.cssClass}>
+                                                <a className="dropdown-item"
+                                                   href="#">Action</a>
+                                                <a className="dropdown-item"
+                                                   href="#">Another Action</a>
+                                                <a className="dropdown-item"
+                                                   href="#">Something else here</a>
+                                            </div>
                                         </li>
                                     </ul>
                                 </div>
