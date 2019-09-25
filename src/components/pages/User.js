57e5e833d08d1932bcc4d6a68e2e3336c66aecbd
Mike Ottinger <mikeottinger@gmail.com>
2019-09-24 21:52:54 -0700

    Layout for new user form

--- src/components/pages/User.js
+++ src/components/pages/User.js
@@ -1,15 +1,106 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import {Card} from "../Card";
+import {Field, Form, Formik} from "formik";
 
 export const User = () => {
 
-    useEffect(() => {
-        // worksService.getWorks().then(works => {
-        //     console.log(works);
-        // });
-        console.log('in User useEffect');
-    });
+    const [isUserFormVisible, setUserFormVisible] = useState('');
+
+    const onNewUserClick = (e) => {
+        console.log('new');
+        e.preventDefault();
+        const isVisible = isUserFormVisible === '' ? 'visible' : '';
+        setUserFormVisible(isVisible);
+    };
 
     return (
-        <div>I am in the user</div>
+        <div className={'row'}>
+            <div className={'col-lg-12'}>
+                <Card title={'User Management'}>
+                    {() => (
+                        <div>
+                            <div className={'row'}>
+                                <div className={'col-lg-2'}>
+
+                                    <button className={'btn btn-info'} onClick={onNewUserClick}>
+                                        <i className={'nc-icon nc-simple-add'}/>
+                                        New User
+                                    </button>
+
+                                </div>
+
+                                <div className={'col-lg-6 new-user ' + isUserFormVisible}>
+                                    <Formik
+                                        initialValues={{username: ''}}
+                                        onSubmit={(values, { setSubmitting, setStatus }) => {
+                                            setSubmitting(false);
+                                        }}
+                                    >
+                                        {({
+                                            values,
+                                            status,
+                                            errors,
+                                            touched,
+                                            handleChange,
+                                            handleBlur,
+                                            handleSubmit,
+                                            isSubmitting
+                                        }) => (
+                                            <Form>
+                                                <div className={'row'}>
+                                                    <div className={'col-lg-6'}>
+                                                        <div className={'form-group'}>
+                                                            <label htmlFor='username'>Username</label>
+                                                            <Field type='text' name='username' className={'form-control'}/>
+                                                        </div>
+                                                    </div>
+                                                    <div className={'col-lg-6'}>
+                                                        <div className={'form-group'}>
+                                                            <label htmlFor='email'>Email</label>
+                                                            <Field type='email' name='email' className={'form-control'}/>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className={'row'}>
+                                                    <div className={'col-lg-6'}>
+                                                        <div className={'form-group'}>
+                                                            <label htmlFor='firstname'>First Name</label>
+                                                            <Field type='text' name='firstname' className={'form-control'}/>
+                                                        </div>
+                                                    </div>
+                                                    <div className={'col-lg-6'}>
+                                                        <div className={'form-group'}>
+                                                            <label htmlFor='lastname'>Last Name</label>
+                                                            <Field type='text' name='lastname' className={'form-control'}/>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className={'row'}>
+                                                    <div className={'col-lg-6'}>
+                                                        <div className={'form-group'}>
+                                                            <label htmlFor='password1'>Password</label>
+                                                            <Field type='password' name='password1' className={'form-control'}/>
+                                                        </div>
+                                                    </div>
+                                                    <div className={'col-lg-6'}>
+                                                        <div className={'form-group'}>
+                                                            <label htmlFor='password2'>Confirm Password</label>
+                                                            <Field type='password' name='password2' className={'form-control'}/>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </Form>
+                                        )}
+
+                                    </Formik>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </Card>
+            </div>
+        </div>
     )
 };
