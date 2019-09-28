f6cc072a23a127d688ceede0435b272d5dda9931
Mike Ottinger <mikeottinger@gmail.com>
2019-09-28 16:55:37 -0700

    Added mysql libs for migrations

---
+++ backend/knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      user: 'cdgd',
+      password: 'password',
+      database: 'cdgd'
+    }
+  }
+};
---
+++ backend/migrations/20190928143710_create_user_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+
+    return knex.schema.createTable('users', function(t) {
+        t.increments('id').primary();
+        t.string('userName').notNullable();
+        t.string('firstName').notNullable();
+        t.string('lastName').notNullable();
+        t.string('password').notNullable();
+        t.timestamps(false, true);
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+};
