d24f98a619bef745b6c1ef1fd0fbbe711b3c7921
Mike Ottinger <mikeottinger@gmail.com>
2019-10-11 09:13:36 -0700

    Refactor NewUser validation as well as the FeedbackPanel component

--- src/components/forms/FeedbackPanel.js
+++ src/components/forms/FeedbackPanel.js
@@ -2,12 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {ErrorMessage} from 'formik';
 
-const cssClassesByProp = show => {
-
-    const baseClasses = 'alert alert-success text-center fade ';
-    return show ? baseClasses + 'show' : baseClasses + 'hide';
-};
-
 export const FeedbackPanel = props => {
 
     return (
