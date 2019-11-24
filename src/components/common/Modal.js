0abd5c3e76e66d296a7f0e570ae8809289459e96
Mike Ottinger <mikeottinger@gmail.com>
2019-11-24 15:47:03 -0800

    eslint fixes

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,39 +1,64 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Modal as ReactModal} from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import {Form} from 'formik';
+import React from "react";
+import * as PropTypes from "prop-types";
+import { Modal as ReactModal } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { Form } from "formik";
 
-export const Modal = ({title, children, show, handleClose, handleAction, submitter, submitLabel}) => {
-	
-	const renderSubmit = () => (
-		submitter ? submitter() :
-			<Button className={'btn btn-info'} variant="primary" onClick={handleAction}>{submitLabel}</Button>
-	);
-	
-	return (
-		<ReactModal dialogClassName={'modal-90w'} show={show} onHide={handleClose} animation={false} backdrop={'static'}>
-			<Form>
-				<ReactModal.Header>
-					<ReactModal.Title>{title}</ReactModal.Title>
-				</ReactModal.Header>
-				<ReactModal.Body>
-					{children}
-				</ReactModal.Body>
-				<ReactModal.Footer>
-					<Button variant="secondary" onClick={handleClose}>Close</Button>
-					{renderSubmit()}
-				</ReactModal.Footer>
-			</Form>
-		</ReactModal>
-	)
+const Modal = ({
+  title,
+  show,
+  handleClose,
+  handleAction,
+  children,
+  submitter,
+  submitLabel
+}) => {
+  const renderSubmit = () =>
+    submitter ? (
+      submitter()
+    ) : (
+      <Button className="btn btn-info" variant="primary" onClick={handleAction}>
+        {submitLabel}
+      </Button>
+    );
+
+  return (
+    <ReactModal
+      show={show}
+      onHide={handleClose}
+      animation={false}
+      backdrop="static"
+    >
+      <Form>
+        <ReactModal.Header>
+          <ReactModal.Title>{title}</ReactModal.Title>
+        </ReactModal.Header>
+        <ReactModal.Body>{children}</ReactModal.Body>
+        <ReactModal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          {renderSubmit()}
+        </ReactModal.Footer>
+      </Form>
+    </ReactModal>
+  );
 };
 
 Modal.propTypes = {
-	title: PropTypes.string,
-	submitLabel: PropTypes.string,
-	show: PropTypes.bool,
-	handleClose: PropTypes.func,
-	handleAction: PropTypes.func,
-	submitter: PropTypes.func
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  submitLabel: PropTypes.string,
+  show: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  handleAction: PropTypes.func,
+  submitter: PropTypes.func
 };
+
+Modal.defaultProps = {
+  submitLabel: "Submit",
+  handleAction: null,
+  submitter: null
+};
+
+export default Modal;
