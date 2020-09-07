14e1e07acca6cc7682c26333c3eef8b2b52ddda0
Mike Ottinger <mikeottinger@gmail.com>
2020-09-06 17:43:31 -0700

    Remove return statement

--- backend/migrations/20191129202748_create_client_table.js
+++ backend/migrations/20191129202748_create_client_table.js
@@ -1,13 +1,10 @@
-exports.up = knex => {
-  return knex.schema.createTable("clients", t => {
+exports.up = knex =>
+  knex.schema.createTable("clients", t => {
     t.increments("id").primary();
     t.string("email").notNullable();
     t.string("firstName").notNullable();
     t.string("lastName").notNullable();
     t.timestamps(false, true);
   });
-};
 
-exports.down = knex => {
-  return knex.schema.dropTable("clients");
-};
+exports.down = knex => knex.schema.dropTable("clients");
